46c0368e043cfc8bedaf78a77e6300be11c45c09
omarchie <100832222+omarchie@users.noreply.github.com>
2023-10-24 01:00:59 -0500

    JS Template
    Removed the static HTML, and built the forecast row with JS instead.

--- JS/script.js
+++ JS/script.js
@@ -5,8 +5,22 @@
 //let apiURL =`https://api.openweathermap.org/data/2.5/weather?q=${placeCity}&units=metric`;
 
 //function to get weather information for the location inputted
-function displayTemperature(response){
-console.log(response)
+function getCityLocationInfo(coordinates)
+{
+  console.log(coordinates);
+  let apiKey = "c819171fe0abdc14039af4ef5dda283b";
+  //let apiURL = `api.openweathermap.org/data/2.5/forecast/daily?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiURL = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiURL);
+  axios.get(apiURL).then(displayTemperature);
+}
+
+//This function is getting the coordinates so that we can display the temperature for the 
+//next few days
+
+function displayTemperature(response)
+  {
+    console.log(response)
     let theCurrentTemp = Math.round(response.data.main.temp);
     let tempDisplay = document.getElementById("thecurrent-temp");
     tempDisplay.innerText = `${theCurrentTemp}ºC`;
@@ -14,29 +28,61 @@ console.log(response)
     let humidity = document.querySelector(".wind-percip li#humidity");
     humidity.innerText = "Humidity : " + response.data.main.humidity +"%";
     let country = response.data.sys.country;
-  let countryDisplay = document.querySelector("#country");
-  countryDisplay.innerText = `${country}`; 
-let wind = Math.round(response.data.wind.speed);
-let windSpeed = document.getElementById("wind");
-windSpeed.innerText = `Wind: ${wind} km/h`;
-let feels = Math.round(response.data.main.feels_like);
-let feelsLike= document.getElementById("feels-like");
-feelsLike.innerText = `Feels Like: ${feels}ºC`;
-let iconImage = document.querySelector("#weather-icon");
-iconImage.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@4x.png`);
-
+    let countryDisplay = document.querySelector("#country");
+    countryDisplay.innerText = `${country}`; 
+    let wind = Math.round(response.data.wind.speed);
+    let windSpeed = document.getElementById("wind");
+    windSpeed.innerText = `Wind: ${wind} km/h`;
+    let feels = Math.round(response.data.main.feels_like);
+    let feelsLike= document.getElementById("feels-like");
+    feelsLike.innerText = `Feels Like: ${feels}ºC`;
+    let iconImage = document.querySelector("#weather-icon");
+    iconImage.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@4x.png`);
+    //let lat = response.data.coord.lat;
     //axios.get(`${apiURL}&appid=${apiKey}`).then(displayTemperature);
-}
+
+    getCityLocationInfo(response.data.coord);
+    //this calls the function to grab the coordinates
+  }
+
 
 function typeLocationStore(position)
  {
-      let apiKey = "f3887e262c88d1158f7e2ef4998e234c";
-     let city = document.getElementById("location-input").value;
-     let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-
+    //console.log(position);
+    let apiKey = "f3887e262c88d1158f7e2ef4998e234c";
+    let city = document.getElementById("location-input").value;
+    let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    
     axios.get(apiURL).then(displayTemperature);
 }
+//this function is getting the "Current Temperature for the inputted City"
+function displayForecast() { 
 
+let forecastInfo = document.querySelector("#forecast-info");
+
+  let days = ["Sun", "Mon", "Tues", "Wed"];
+
+      let forecastHTMLValue = `<div class="row">`;
+
+      days.forEach(function (day) {
+      forecastHTMLValue  = forecastHTMLValue  +
+      
+       `
+          <div class="col-2">
+          <div class="pic-forecast"> ☀ </div>
+          <div class= "weather-five-day"> ${day}</div>
+          <div class="temp-days-forecast">  
+               <span class="weather-forecast-temp-max"> 20ºC</span>
+                <span class="weather-forecast-temp-min"> 10ºC</span>
+          </div>
+          </div>
+       `;
+});
+//concatenate the the HTML so  that you can create a row
+forecastHTMLValue = forecastHTMLValue + `</div>`;
+forecastInfo.innerHTML = forecastHTMLValue;
+
+}
 function searchLocation(event)
  {
     event.preventDefault();
@@ -110,5 +156,5 @@ weatherDate.innerText = `${day}, ${month} ${date} ${year} - ${time}`;
 //Retrieve the city inputted to get the city for the weather 
 
 
-
+displayForecast();
 //navigator.geolocation.getCurrentPosition(typeLocationStore);
